16fa82415f8027431bfe80cc6df7157eee856ec9
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-16 01:11:06 +0530

    adding controller and testing a token for getting user

---
+++ server/Controller/auth_controller.js
@@ -0,0 +1,52 @@
+
+import User from "../Models/User.js";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+//for getting all users
+const getuser = async (req, res) => {
+    try {
+        const users = await User.find();
+        if (!users) {
+            res.status(404).json(error, error.message);
+        }
+       const jwt_key = process.env.JWT_KEY;
+       const token = jwt.sign(
+             { _id:users.id ,name:users.name
+                
+            } ,jwt_key,{expiresIn:"1d"});
+
+
+       
+    return res.status(200).json({
+      success: true,
+      users: users.map(u => ({
+        _id: u._id,
+        name: u.name,
+        email: u.email
+      }))
+    });
+
+    } catch (error) {
+      return res.status(500).json({ success:false ,error: error.message });
+
+    }
+};
+
+//for creating a new user
+const createUSer = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const newuser = new User({ name, email, password });
+        await newuser.save();
+        res.status(201).json(newuser);
+
+    } catch (error) {
+
+        res.status(500).json({ error: error.message });
+
+    }
+}
+
+
+export default { getuser, createUSer };
--- server/src/index.js
+++ server/src/index.js
@@ -1,43 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import User from "../Models/User.js";
+import authController from "../Controller/auth_controller.js";
+import connectDb from "../Database_connection/dbconnection.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8535;
 
+connectDb();
+
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.DB_URL)
-.then(()=> console.log("Connected to MongoDB"))
-.catch((err) => console.log("Error connecting to MongoDB:", err));
-
-
 app.get("/",(req,res)=>{res.send("server is running ");});
-app.get("/User", async(req,res)=>{
-    try{
-        const users= await User.find();
-        res.json(users);
-    }catch(err){
-        res.status(500).json({error: err.message});
-        }
-});
+app.get("/User", authController.getuser);
 
 
-app.post("/User",async(req,res)=>{
- try {
-    const {name,email,password}=req.body;
-    const newuser = new User({name,email,password});
-    await newuser.save();
-    res.status(201).json(newuser);
-    
- } catch (error) {
-    res.status(500).json({error: error.message});
- }
-});
+app.post("/User",authController.createUSer);
 
 app.listen(PORT ,()=>{console.log(`server is running on port ${PORT}`)});
