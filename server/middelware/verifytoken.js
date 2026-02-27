d8c198531f54ca67ea5789a6d5eb350ecea9058d
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-27 17:17:04 +0530

    fix: improve authmiddleware.js

--- server/middelware/verifytoken.js
+++ server/middelware/verifytoken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const authmiddleware = (req,res,next) => {
-    const header = req.cookies.token;
+    const token = req.cookies.token;
 
     if (!token) {
         return res.status(401).json({success:false, message: "No token" });
