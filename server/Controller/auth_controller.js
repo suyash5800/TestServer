1fad89107dbcb8178e90bf1d182bdbf161b75e71
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-27 13:34:37 +0530

    improving a status code

--- server/Controller/auth_controller.js
+++ server/Controller/auth_controller.js
@@ -33,7 +33,7 @@ const createUser = async (req, res) => {
         }
         const existing = await User.findOne({ email });
         if (existing) {
-            return res.status(400).json({ message: "Email already in use" });
+            return res.status(404).json({ message: "Email already in use" });
         }
 
         const hashpass = await bcrypt.hash(password, 10);
