95307c8ddd961f9c71a7b834e551e7b19a7c83a6
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-27 14:59:11 +0530

    Add:trailing sash

--- server/src/index.js
+++ server/src/index.js
@@ -28,7 +28,8 @@ app.use(cors({
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 app.use(cookieParser())
 app.use(express.json());
