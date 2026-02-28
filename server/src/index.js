6d831086d408195f8fbf302978c1c8294ccfc524
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-28 23:13:22 +0530

    add: /

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ connectDb();
 
 const allowedOrigins = [
   "http://localhost:5173/",
-  "https://um-stay-client.vercel.app"
+  "https://um-stay-client.vercel.app/"
 ];
 app.options("*", cors());
 app.use(cookieParser())
