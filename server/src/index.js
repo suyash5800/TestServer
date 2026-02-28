00bdac21781133090d9cfb4a2de6c1cc2b6932f5
Suyash Ranchandra Patil <suyapatil5800@gmail.com>
2026-02-28 18:36:12 +0530

    trying to solve cookie error

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ connectDb();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://um-stay-client.vercel.app/"
+  "https://um-stay-client.vercel.app"
 ];
 app.use(cookieParser())
 app.use(express.json());
